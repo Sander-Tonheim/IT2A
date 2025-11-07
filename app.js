e36cbdb757dc9feb84a58a95e32adf31bbf207d7
Sander Tonheim <tonheims98@gmail.com>
2025-11-07 11:25:29 +0100

    added comments

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
+// importerer pakker.
 const express = require("express");
-const app = express();
-const port = 3000;
 const mysql = require("mysql2/promise");
+
+const app = express();
+
+// Definerer hvilken port som skal være åpen for å motta forespørsler (req) fra klient.
+const port = 3000;
+// importerer funkjson som lager kobling til databasen.
 const { createConnection } = require("./database/database");
+
+// importerer funkjson som henter data fra databasen.
 const { getCar } = require("./database/services");
 
+// konfigurerer EJS som malmotor.
 app.set("view engine", "ejs");
 
-app.get("/", async (req, res) => {
-	const connection = await createConnection();
-	const results = await getCar(connection);
+// serverer statiske filer.
+app.use(express.static("public"));
 
+// forteller hvor landingssiden er
+app.get("/", async (req, res) => {
+	// åpner en ny mysql tilkobling
+	const connection = await createConnection();
+	// henter data fra databasen.
+	const results = await getCar(connection);
+	// definerer hvordan vi skal svare på forsepørslen (req) fra klienten på denne ruten.
 	res.render("index", { cars: results });
 });
-
-app.use(express.static("public"));
 
 app.get("/about", (req, res) => {
 	res.render("about");

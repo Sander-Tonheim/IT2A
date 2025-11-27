9bd7d3d274ffe0f2939fe99a4a3a6fbe5ab72518
Sander Tonheim <tonheims98@gmail.com>
2025-11-27 11:39:24 +0100

    added insert into database from form func

--- database/services.js
+++ database/services.js
@@ -4,4 +4,9 @@ async function getCar(connection) {
 	return results;
 }
 
-module.exports = { getCar };
+async function insertIntoCarDatabase(connection, brand, engine, wheels) {
+	console.log(brand, engine, wheels);
+	const query = "INSERT INTO car (brand, engine, wheels) VALUES (?, ?, ?)";
+	return await connection.execute(query, [brand, engine, wheels]);
+}
+module.exports = { getCar, insertIntoCarDatabase };

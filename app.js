e016adf8356ad07efcc333b1dc20e6ed8183bce9
Sander Tonheim <tonheims98@gmail.com>
2026-02-17 12:37:08 +0100

    imported express-layout

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
 const session = require("express-session");
+const expressLayouts = require("express-ejs-layouts");
 
 const app = express();
 
